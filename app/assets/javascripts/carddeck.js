0ca39c2136b529da42ad0ebf93d912ab441f1089
Zack Nagler <zachary.nagler@gmail.com>
2014-11-07 15:48:29 -0500

    wip

---
+++ app/assets/javascripts/carddeck.js
@@ -0,0 +1,32 @@
+
+
+var exampleDeck = {deck:[
+	{q:"hello" ,a:"hola" },
+	{q: "bye",a:"adios"},
+	{q: "dog",a: "perro"},
+	{q: "cat",a: "gato"},
+	{q: "food",a: "comida"},
+	]}
+
+
+
+function CardDeck(carddeck_object){
+	this.solvedDeck = false
+	this.remainingCards = carddeck_object.deck
+	this.previousCorrect = false
+	this.currentCard = this.remainingCards[0]
+	this.advanceToNextCard = function(){
+		this.remainingCards = this.remainingCards.slice(1)
+		this.currentCard = this.remainingCards[0]
+		if (this.remainingCards.length === 0){
+			this.solvedDeck = true
+		}
+	}
+	this.shuffleCards = function(){
+		this.remainingCards = shuffle(this.remainingCards)
+		this.currentCard = this.remainingCards[0]
+	}
+	// this.parseInput = function(){}
+
+}
+
