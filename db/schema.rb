3e9015aebb0000b0359194a558c0ea73232ec64f
Zachary Nagler <zachary.nagler@gmail.com>
2014-11-08 15:16:12 -0500

    added two deck arrays two seed file.  Rake db:seed now works with those two sets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107155207) do
+ActiveRecord::Schema.define(version: 20141108192841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20141107155207) do
     t.integer  "quizlet_modified_date"
     t.json     "cards"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "included_decks", force: true do |t|
+    t.string   "title"
+    t.integer  "term_count"
+    t.json     "cards"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
