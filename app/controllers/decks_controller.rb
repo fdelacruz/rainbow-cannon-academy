73c2179597485e805f5b4f9f673e8f7d9de1bd15
Joe McGrath & Francisco De La Cruz <pair+e710476.fdelacruz@devbootcamp.com>
2014-11-07 21:36:51 -0500

    able to detect changes from quizlet api and update users decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -8,8 +8,8 @@ class DecksController < ApplicationController
   def update
     array_of_deck_hashes_from_quizlet = get_decks(session['uid'], session['access_token'])
     array_of_deck_objects_from_db = User.find(session[:user_id]).decks
-    # quizlet_array_of_deck_id_and_modified_date = array_of_deck_hashes_from_quizlet.map {|deck| deck.slice("id", "modified_date")}
-    # db_array_of_deck_id_and_modified_date = User.first.decks.map {|deck| {"id" => deck.quizlet_deck_id, "modified_date" => deck.quizlet_modified_date} }
+    quizlet_array_of_deck_id_and_modified_date = array_of_deck_hashes_from_quizlet.map {|deck| deck.slice("id", "modified_date")}
+    db_array_of_deck_id_and_modified_date = User.first.decks.map {|deck| {"id" => deck.quizlet_deck_id, "modified_date" => deck.quizlet_modified_date} }
     unless quizlet_array_of_deck_id_and_modified_date == db_array_of_deck_id_and_modified_date
       create_decks_for(User.find(session[:user_id]), array_of_deck_hashes_from_quizlet)
     end
--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -7,26 +7,8 @@ module DecksHelper
 
   def create_decks_for(user, array_of_deck_hashes)
     array_of_deck_hashes.each do |deck|
-      # title = deck['title']
-      # id = deck['id']
-      # term_count = deck['term_count']
-      # modified_date = deck['modified_date']
-      cards = deck['terms']
-      cards_json = deck['terms'].class
-      binding.pry
       db_deck = Deck.create(title: deck['title'], quizlet_modified_date: deck['modified_date'], quizlet_deck_id: deck['id'], term_count: deck['term_count'], cards: deck['terms'])
       user.decks.push(db_deck)
-      # create_cards_from_array_of_card_hashes(db_deck, deck['terms'])
     end
   end
-
-  # def create_cards_from_array_of_card_hashes(deck_object, array_of_card_hashes)
-  #   array_of_card_hashes.each do |card|
-  #       id = card['id']
-  #       term = card['term']
-  #       definition = card['definition']
-  #       deck_object.cards.push(Card.create(quizlet_card_id: id, term: term, definition: definition))
-  #     end
-  # end
-
 end
