3c79f506d4cb61fbec61821a59eee63e37038f9a
Joe McGrath & Zachary Nagler <pair+e710476.znagler@devbootcamp.com>
2014-11-09 16:00:12 -0500

    alter js assets to be relative so they can be loaded in on the deck show and included deck show pages

--- app/assets/javascripts/lifecycle_preload.js
+++ app/assets/javascripts/lifecycle_preload.js
@@ -1,11 +1,11 @@
 phaserLifeCycleFunctions.preload = function(){
-  game.load.image('sky', 'assets/sky.png')
-  game.load.image('ground', 'assets/platform.png')
-  game.load.image('star', 'assets/star.png')
-  game.load.image('dude', 'assets/ship.png')
-  game.load.spritesheet('invader', 'assets/baddie.png', 32, 32)
-  game.load.spritesheet('rain', 'assets/rain.png', 17, 17)
-  game.load.image('bullet', 'assets/bullets.png')
+  game.load.image('sky', '../assets/sky.png')
+  game.load.image('ground', '../assets/platform.png')
+  game.load.image('star', '../assets/star.png')
+  game.load.image('dude', '../assets/ship.png')
+  game.load.spritesheet('invader', '../assets/baddie.png', 32, 32)
+  game.load.spritesheet('rain', '../assets/rain.png', 17, 17)
+  game.load.image('bullet', '../assets/bullets.png')
 }
 
 
