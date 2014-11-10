1e8bfd50c63ba86c74bfabd4751bfbd27ff4c440
Zack Nagler <zachary.nagler@gmail.com>
2014-11-09 19:22:59 -0500

    fixed paths of assets

--- app/assets/javascripts/lifecycle_preload.js
+++ app/assets/javascripts/lifecycle_preload.js
@@ -1,12 +1,12 @@
 phaserLifeCycleFunctions.preload = function(){
-  game.load.image('sky', 'assets/sky.png')
-  game.load.image('ground', 'assets/platform.png')
-  game.load.image('star', 'assets/star.png')
-  game.load.image('dude', 'assets/ship.png')
-  game.load.image('diamond', 'assets/diamond.png')
-  game.load.spritesheet('invader', 'assets/baddie.png', 32, 32)
-  game.load.spritesheet('rain', 'assets/rain.png', 17, 17)
-  game.load.image('bullet', 'assets/bullets.png')
+  game.load.image('sky', '../assets/sky.png')
+  game.load.image('ground', '../assets/platform.png')
+  game.load.image('star', '../assets/star.png')
+  game.load.image('dude', '../assets/ship.png')
+  game.load.image('diamond', '../assets/diamond.png')
+  game.load.spritesheet('invader', '../assets/baddie.png', 32, 32)
+  game.load.spritesheet('rain', '../assets/rain.png', 17, 17)
+  game.load.image('bullet', '../assets/bullets.png')
 }
 
 
