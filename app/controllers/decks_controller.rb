16d9d46349c74f0800f968f7456df5cf237b2032
e710476 <josephmcgrathva@gmail.com>
2014-11-10 10:42:55 -0500

    rename get_decks helper method to be more descript

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,14 +1,14 @@
 class DecksController < ApplicationController
   include DecksHelper
   def create
-    array_of_deck_hashes = get_decks(session['uid'], session['access_token'])
+    array_of_deck_hashes = get_users_decks_from_quizlet(session['uid'], session['access_token'])
     create_decks_for(User.find(session[:user_id]), array_of_deck_hashes)
     redirect_to root_path
   end
 
   def update
     user = User.find(session[:user_id])
-    array_of_deck_hashes_from_quizlet = get_decks(session['uid'], session['access_token'])
+    array_of_deck_hashes_from_quizlet = get_users_decks_from_quizlet(session['uid'], session['access_token'])
     array_of_deck_objects_from_db = user.decks
     update_decks_in_db(array_of_deck_hashes_from_quizlet, array_of_deck_objects_from_db, user)
     redirect_to root_path
--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,7 +1,11 @@
 module DecksHelper
 
-  def get_decks(uid, access_token)
+  def get_users_decks_from_quizlet(uid, access_token)
     JSON.parse(`curl -H "Authorization: Bearer #{access_token}" "https://api.quizlet.com/2.0/users/#{uid}/sets"`)
+  end
+
+  def get_decks_from_users_classes_from_quizlet(uid, access_token)
+
   end
 
 
