629b3cab06258c8a84291f56e7becbdf4b1fc02e
Zack Nagler <zachary.nagler@gmail.com>
2014-11-11 15:32:54 -0500

    remove cursors from flashcard.js

--- app/assets/javascripts/flashcard.js
+++ app/assets/javascripts/flashcard.js
@@ -7,22 +7,11 @@ flashCard.prototype = {
   },
 
   update: function() {
-
+  	
    	var player = gameState.player
-	  var cursors = gameState.cursors
-
 
 	  // set scroll speed of background
 	  starfieldBackground.update()
-	  // ---
-
-	  // overallUI.checkIfFlashcardsComplete()
-	  // if (gameState.currentDeck.cardsLeftInCurrentRound() < 1 ) {
-	  // 	flashCardUI.clearFlashCardText()
-	  //   gameState.currentLevel++
-	  //   // console.log("go to fight mode")
-	  //   // game.state.start('fight')
-	  // }
 
   },
   render: function() {
