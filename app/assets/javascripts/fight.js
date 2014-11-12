db43f1554293df2bb2d004ccf956ed31c5e01d3f
e710476 <josephmcgrathva@gmail.com>
2014-11-12 14:56:25 -0500

    player loses lives upon death - able to switch into game over conditional when lives = 0

--- app/assets/javascripts/fight.js
+++ app/assets/javascripts/fight.js
@@ -161,8 +161,16 @@ fight.prototype = {
 		  }
 
 		  if (gameUI.playerDead(gameState.player)){
-		  	gameState.firstTimeOnLevel = false
-		    game.state.start('level_intro')
+		  	gameState.lifes -= 1
+		  	if (gameState.lifes <= 0){
+		  		console.log('game over')
+		  		// gameState.firstTimeOnLevel = false
+		  		// game.state.start('game_over')
+		  	} else {
+		  		gameState.firstTimeOnLevel = false
+		    	game.state.start('level_intro')
+		  	}
+
 		  }
 
 		  if (gameState.groups.aliens.x < 850){
